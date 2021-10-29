a6a2d61e2b8c9477898f3e1378e46490a552a3a4
aivaraskriksciunas <aiviskri@gmail.com>
2021-10-29 14:06:28 +0300

    Updated 404 error page

--- web/wp/wp-content/themes/supportandfeed/404.php
+++ web/wp/wp-content/themes/supportandfeed/404.php
@@ -1,1 +1,25 @@
-Page not found
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Aivaras Kriksciunas">
+
+    <?php wp_head() ?>
+</head>
+<body id='error-page'>
+    
+    <div class='min-h-screen w-full flex items-stretch justify-center p-10 bg-black'>
+        <div id='bg-image' class='w-full flex items-center justify-center' style='background-image: url("<?= sf_get_asset( 'images/error-page-bg.jpg' ); ?>")'>
+            <div class='w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-10 bg-black text-white text-center'>
+                <h1 class='uppercase text-8xl text-center shadow-2xl'>page not found</h1>
+
+                <a href='/' class='text-xs font-light mt-4 inline-block text-gray-200 hover:underline'>Back to homepage</a>
+            </div>
+        </div>
+    </div>
+    
+<?php wp_footer(); ?>
+</body>
+</html>
--- web/wp/wp-content/themes/supportandfeed/page-about.php
+++ web/wp/wp-content/themes/supportandfeed/page-about.php
@@ -12,24 +12,6 @@
     </div>
 </div>
 
-<!-- <div id='cover' class='bg-white'>
-    <div class="container lg:flex items-center min-h-screen ">
-        <div id='cover-title' class='lg:w-1/2 py-20'>
-            <div>
-                <h1 class=' text-8xl italic uppercase mb-8'><?= the_title() ?></h1>
-
-                <div class='leading-relaxed text-justify'>
-                    <?= get_post_meta( get_the_ID(), 'page_subtitle', true ) ?>
-                </div>
-            </div>
-        </div>
-        <div class='lg:w-1/2 px-8'>
-            <img src='<?= wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ) ?>'>
-        </div>
-    </div>
-    
-</div> -->
-
 <div class='bg-gray-100 w-full inline-block'>
     
     <h1 class='mt-20 mb-10 text-center text-7xl'>
